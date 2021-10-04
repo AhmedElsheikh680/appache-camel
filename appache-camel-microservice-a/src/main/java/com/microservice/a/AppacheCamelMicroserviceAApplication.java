7b7cfc6cc20f667742ae38ef25d8fefb495763d3
ahmed.elsheikh <ahmed.elsheikh>
2021-10-04 14:09:37 +0200

    init project a

--- appache-camel-microservice-a/src/main/java/com/microservice/a/AppacheCamelMicroserviceAApplication.java
+++ appache-camel-microservice-a/src/main/java/com/microservice/a/AppacheCamelMicroserviceAApplication.java
@@ -3,6 +3,7 @@ package com.microservice.a;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AppacheCamelMicroserviceAApplication {
 
